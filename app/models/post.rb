634144c2fb8b24d7f1b42fb35b1529bb305add7e
yosaddis <email.to.yoseph@gmail.com>
2023-08-19 23:21:15 +0300

    Fix linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,6 @@ class Post < ApplicationRecord
   validates :comments_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
   validates :likes_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
-
   after_save :update_counters
 
   def last_comments
