f5d5690d48b2a86381f3d27cb305f89cb99fc9b6
yosaddis <email.to.yoseph@gmail.com>
2023-08-19 23:03:06 +0300

    Add validation to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,11 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :title, presence: { message: 'Title can\'t be empty' }, length: { maximum: 250 }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+
+
   after_save :update_counters
 
   def last_comments
