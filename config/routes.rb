cf48afc2296d64353130fc9b0ed0849a34c45823
yosaddis <email.to.yoseph@gmail.com>
2023-08-22 16:14:36 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
 
   get "/users", to: "users#index"
   get "/users/:id", to: "users#show"
-  get "/users/:user_id/posts", to: "posts#index"
-  get "/users/:user_id/posts/:id", to: "posts#show"
+  get "/users/:user_id/posts", to: "posts#index", as: 'user_posts'
+  get "/users/:user_id/posts/:id", to: "posts#show", as: 'user_post'
 
 
   # Defines the root path route ("/")
