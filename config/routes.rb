ff99263157760610e719a178c0be2c9f43ef88e0
yosaddis <email.to.yoseph@gmail.com>
2023-08-25 14:51:40 +0300

    Updates routes with the new routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,11 @@ Rails.application.routes.draw do
   get "/users/:user_id/posts", to: "posts#index", as: 'user_posts'
   get "/users/:user_id/posts/:id", to: "posts#show", as: 'user_post'
 
+  get '/posts/new', to: 'posts#new', as: 'new_post'
+  post '/posts/new', to: 'posts#create', as: 'create_post'
+  get '/comment/new/:post_id', to: 'comments#new', as: 'new_comment'
+  post '/comment/new/:post_id', to: 'comments#create', as: 'create_comment'
+  post '/likes/new', to: 'likes#create', as: 'create_like'
 
   # Defines the root path route ("/")
   # root "articles#index"
