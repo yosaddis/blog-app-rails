7069c13a0fa647b7c18b373b5065a25db6503d3b
yosaddis <email.to.yoseph@gmail.com>
2023-08-25 14:50:43 +0300

    update posts controller for create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,4 +13,27 @@ class PostsController < ApplicationController
       @post = nil
     end
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(author_id: current_user.id, title: params[:title], text: params[:text])
+
+    if @post.save
+      flash[:notice] = 'Post has been created!'
+      redirect_to user_post_path(current_user.id, @post.id)
+
+    else
+      flash.now[:alert] = 'Post has not been created.'
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
