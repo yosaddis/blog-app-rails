ddf30f3473297639289fa45c9e2ba54a1f83d6ac
yosaddis <email.to.yoseph@gmail.com>
2023-08-30 14:13:21 +0300

    Update the controller for N+1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,12 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = Post.where(author_id: @user.id)
+    @posts = @user.posts
   end
 
   def show
-    @user = User.find(params[:user_id])
-
-    begin
-      @post = Post.where(author_id: @user.id).find(params[:id])
-    rescue StandardError
-      @post = nil
-    end
+      #@post = Post.find(params[:id])
+      @post = Post.includes(:author, :comments, :likes).find(params[:id])
   end
 
   def new
