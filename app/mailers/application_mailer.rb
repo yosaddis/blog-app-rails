67f0778287a08a879752213f3d5199f49daaffc2
yosaddis <email.to.yoseph@gmail.com>
2023-09-02 03:01:14 +0300

    Add configration for device mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'email.to.yoseph+blog-app-rails@gmail.com'
   layout 'mailer'
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,5 +66,20 @@ Rails.application.configure do
   # config.action_view.annotate_rendered_view_with_filenames = true
 
   # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
+  config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = { from: "email.to.yoseph@gmail.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name:"email.to.yoseph@gmail.com ",
+    password: "qphlyizcfrbyhutm",
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
