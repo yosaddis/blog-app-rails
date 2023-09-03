17ceefe308af6f33eb15b5b4c76ecf29ed9206cf
yosaddis <email.to.yoseph@gmail.com>
2023-09-03 05:52:31 +0300

    Fixed linter messages

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,6 +1,6 @@
 class Api::PostsController < ActionController::Base
-    def index
-        @posts = Post.includes(:comments).where(author_id: params[:user_id])
-        render json: @posts
-    end
+  def index
+    @posts = Post.includes(:comments).where(author_id: params[:user_id])
+    render json: @posts
+  end
 end
